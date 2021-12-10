0c8b4ce21333f814d79588c1c1773dd3291e0aa8
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2021-12-10 21:32:26 +0300

    Implement SQS and SNS

--- handler.js
+++ handler.js
@@ -3,11 +3,11 @@ const AWS = require('aws-sdk');
 module.exports = {
   usersSubmit: (event, context, callback) => {
     const sqs = new AWS.SQS();
-    console.log('*************', event)
+    // console.log('*************', event)
     const { users } = JSON.parse(event.body);
     users.forEach(user => {
       sqs.sendMessage({
-        QueueUrl: process.env.SQS_URL,
+        QueueUrl: process.env.SQS_URL, // is subscription here??
         MessageBody: user,
       }, () => {
         console.log(`Send message for: ${user}`);
@@ -15,7 +15,8 @@ module.exports = {
       callback(null, {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*'
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
         }
       })
     })
@@ -23,10 +24,15 @@ module.exports = {
   usersInvite: (event) => {
     const users = event.Records.map(({ body }) => body);
     const sns = new AWS.SNS({ region: 'us-east-1' });
-
     // Create user account
     // Add user to the project
     // Send invitation
-    console.log(users);
+    sns.publish({
+      Subject: 'Your are invited',
+      Message: JSON.stringify(users),
+      TopicArn: process.env.SNS_ARN
+    }, () => {
+      console.log(`Send email for: ${JSON.stringify(users)}`);
+    });
   }
 };
